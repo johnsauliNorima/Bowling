49172c98afb801fe5306454127e8f9d37e097a87
johnsauliNorima <john.sauli@Norimaconsulting,com>
2022-04-29 17:41:58 +0800

    first commit

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        BowlingMethods bowlings= new BowlingMethods();
+        bowlings.Playercounter();
+    }
+}
